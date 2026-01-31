afdb9ffed922f5cc2ce3d59644cdddc8728814c7
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-01-31 19:35:00 -0300

    build: initial build for ms operations

---
+++ ms-operations/src/main/java/com/joaop/ms/MsOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.joaop.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOperationsApplication.class, args);
+	}
+
+}
