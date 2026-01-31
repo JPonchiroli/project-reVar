fd1ff559efd267c1196a3b4ead408e4b8689c4bf
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-01-31 18:43:57 -0300

    build: initial build

---
+++ ms-individuals/src/main/java/com/joaop/ms/MsIndividualsApplication.java
@@ -0,0 +1,13 @@
+package com.joaop.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIndividualsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIndividualsApplication.class, args);
+	}
+
+}
