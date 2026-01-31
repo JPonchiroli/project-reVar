71fc7d0a4b86b344ab3d1b2dbae415b27b291ecb
JPonchiroli <joaopedroponqueroli@gmail.com>
2026-01-31 19:15:29 -0300

    feat: create handler for exceptions

---
+++ ms-individuals/src/main/java/com/joaop/ms/Service/Exceptions/IndividualNotFoundException.java
@@ -0,0 +1,7 @@
+package com.joaop.ms.Service.Exceptions;
+
+public class IndividualNotFoundException extends RuntimeException {
+    public IndividualNotFoundException(String message) {
+        super(message);
+    }
+}
